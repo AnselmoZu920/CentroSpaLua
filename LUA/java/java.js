155308f7a0de5373db6c1c0c70e9176794d67526
Luis Anselmo Zuñiga Armas <94752300+AnselmoZu920@users.noreply.github.com>
2022-12-23 13:33:48 -0600

    Add files via upload

---
+++ LUA/java/java.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>0);
+});
+
+const $form = document.querySelector('#form')
+const $buttonMalito = document.querySelector('#trucazo')
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'))
+    $buttonMalito.setAttribute('href', `mailto:anselmozu90@gmail.com?&subjetc=${form.get('name')}${form.get('mail')}${form.get('number')}&body=${form.get('mensaje')}`)
+    $buttonMalito.click()
+}
