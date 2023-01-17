e6d620e50c1fe1679b276af4a74c802d4b8d1771
Luis Anselmo Zuñiga Armas <94752300+AnselmoZu920@users.noreply.github.com>
2023-01-17 13:20:59 -0600

    Update java.js

--- LUA/java/java.js
+++ LUA/java/java.js
@@ -11,6 +11,6 @@ function handleSubmit(event){
     event.preventDefault()
     const form = new FormData(this)
     console.log(form.get('name'))
-    $buttonMalito.setAttribute('href', `mailto:anselmozu90@gmail.com?&subjetc=${form.get('name')}${form.get('mail')}${form.get('number')}&body=${form.get('mensaje')}`)
+    $buttonMalito.setAttribute('href', `mailto:anselmozu90@gmail.com?subjetc=${form.get('name')} ${form.get('mail')} ${form.get('number')}&body=${form.get('mensaje')}`)
     $buttonMalito.click()
 }
